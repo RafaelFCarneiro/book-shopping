67b9e5a9daba9cc273c25b062ce35fa1cdb13dda
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2017-10-27 23:52:08 -0300

    signup and siginin working with token

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -17,7 +18,8 @@ const appRoutes: Routes = [
     { path: ':id/edit',  component: RecipeEditComponent }
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent }
 ]
 
 @NgModule({
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,3 +1,4 @@
+import { AuthService } from '../auth/auth.service';
 import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 import { Http, Response } from '@angular/http';
@@ -6,17 +7,20 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http, private recipeService: RecipeService,
+    private authService :AuthService) {}
 
   storeRecipes() {
+    const token = this.authService.getToken();
     return this.http.put(
-      this.getUrl('recipes.json'),
+      this.getUrl('/recipes.json', `?auth=${token}`),
       this.recipeService.getRecipes()
     );
   }
 
   getRecipes() {
-    this.http.get(this.getUrl('recipes.json'))
+    const token = this.authService.getToken();
+    this.http.get(this.getUrl('/recipes.json', `?auth=${token}`))
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
@@ -34,7 +38,7 @@ export class DataStorageService {
       );
   }
 
-  private getUrl(append) {
-    return `https://recipe-book-ng-study.firebaseio.com/${append}`
+  private getUrl(append = '', params = '') {
+    return `https://recipe-book-ng-study.firebaseio.com${append}${params}`
   }
 }
