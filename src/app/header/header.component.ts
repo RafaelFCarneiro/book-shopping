3759f29eed6495c362ecb4ff224c3265e96757bb
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2017-10-27 23:58:44 -0300

    add showup rules to manager, login and register links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from '../auth/auth.service';
 import { Response } from '@angular/http';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Component } from '@angular/core';
@@ -8,7 +9,8 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService,
+    private authService: AuthService) {}
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
